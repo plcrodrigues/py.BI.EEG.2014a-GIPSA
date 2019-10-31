7e74435482217fd14e871ea288509bbde4a1e357
Pedro Rodrigues <pedro.rodrigues01@gmail.com>
2019-10-31 14:39:02 +0100

    pushing all files

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name='braininvaders2014',  # Required
+    name='braininvaders2014a',  # Required
 
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
